66ec7d9d8f0ba84fe17882c47cfd02f270335250
LuisaMSegur <165593500+LuisaMSegur@users.noreply.github.com>
2025-04-12 18:51:00 -0400

    cambio variables de entorno

--- app/api/api.js
+++ app/api/api.js
@@ -1,6 +1,6 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
-const BLOG_ID = process.env.NEXT_PUBLIC_BLOG_ID;
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const BLOG_ID = process.env.BLOGGER_BLOG_ID;
+const API_KEY = process.env.BLOGGER_API_KEY;
 
 export async function getBlogById() {
   try {
